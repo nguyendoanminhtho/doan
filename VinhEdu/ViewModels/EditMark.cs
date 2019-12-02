5fab2eab611a53de0391f1d0f58835c0a9530e78
Ndmthao <minhthaonguyendoan@gmail.com>
2019-12-02 15:37:02 +0700

    chức năng xem điểm

--- VinhEdu/ViewModels/EditMark.cs
+++ VinhEdu/ViewModels/EditMark.cs
@@ -6,6 +6,14 @@ using System.Web;
 
 namespace VinhEdu.ViewModels
 {
+    public class ShowMark {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string SubjectName { get; set; }
+        public int SubjectID { get; set; }
+        public Score Score { get; set; }
+        public string TempScore { get; set; }
+    }
     public class EditMark
     {
         public int StudentID { get; set; }
@@ -19,16 +27,27 @@ namespace VinhEdu.ViewModels
     }
     public class Score
     {
-        public int M1 { get; set; }
-        public int M2 { get; set; }
-        public int M3 { get; set; }
-        public int M4 { get; set; }
-        public int M5 { get; set; }
-        public int M6 { get; set; }
-        public int M7 { get; set; }
-        public int T1 { get; set; }
-        public int T2 { get; set; }
-        public int T3 { get; set; }
-        public int K1 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M1 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M2 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M3 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M4 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M5 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M6 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string M7 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string T1 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string T2 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string T3 { get; set; }
+        [RegularExpression(@"^[x]|(([1-9][0-9]{0,1}(\.[\d]{1,2})?|10))$")]
+        public string K1 { get; set; }
     }
 }
